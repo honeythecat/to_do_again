367623a5c143c713f156caa1948eed8255c35375
heather <heather@thisisnotanorg.org>
2015-03-16 11:28:51 -0700

    add edit, new and update logic and erb files

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,4 +8,32 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     # render :show
   end
+
+  def new
+    @list = List.new
+    render :new
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if@list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
+  end
 end
